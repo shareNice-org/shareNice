e9759981b3459f357f00c4f82a3ec5e47457e29d
Mischa Tuffield <mischa@mmt.me.uk>
2011-05-12 13:04:31 +0100

    Fixed the indentation ... no semantic change

--- grab-icons/snarf-icons.pl
+++ grab-icons/snarf-icons.pl
@@ -5,19 +5,21 @@ my $pattern = shift;
 open(DOMAINS, '<', 'domains.txt') || die "cannot open domains file: $!";
 
 while(<DOMAINS>) {
-	chomp;
-	if ($pattern) {
-		next unless /$pattern/;
-	}
-	if (/(.*) \[(.*)\]/) {
-		my $domain = $1;
-		my $url = $2;
-		my $fn = $domain.'.png';
-		system('curl', $url, '-o', '../images/'.$fn);
-	} else {
-		my $fn = $_.'.png';
-		$fn =~ s/\//\./g;
-		$fn =~ s/www\d?\.//g;
-		system('curl', 'http://www.google.com/s2/favicons?domain='.$_, '-o', 'images/'.$fn);
-	}
+    chomp;
+    if ($pattern) {
+        next unless /$pattern/;
+    }
+    if (/(.*) \[(.*)\]/) {
+        my $domain = $1;
+        my $url = $2;
+        my $fn = $domain.'.png';
+        system('curl', $url, '-o', '../images/'.$fn);
+    } else {
+        my $fn = $_.'.png';
+        $fn =~ s/\//\./g;
+        $fn =~ s/www\d?\.//g;
+        system('curl', 'http://www.google.com/s2/favicons?domain='.$_, '-o', 'images/'.$fn);
+    }
 }
+
+# vi:set expandtab sts=4 sw=4:
