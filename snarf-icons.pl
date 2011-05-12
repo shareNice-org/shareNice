e3e41a72da4c569f4edfd5ef38235c62c9c82c02
Steve Harris <steve.harris@garlik.com>
2011-05-12 11:19:19 +0100

    Added script to gather icons, and the icons it's gathered

---
+++ snarf-icons.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -w
+
+my $pattern = shift;
+
+open(DOMAINS, '<', 'domains.txt') || die "cannot open domains file: $!";
+
+while(<DOMAINS>) {
+	chomp;
+	if ($pattern) {
+		next unless /$pattern/;
+	}
+	if (/(.*) \[(.*)\]/) {
+		my $domain = $1;
+		my $url = $2;
+		my $fn = $domain.'.png';
+		system('curl', $url, '-o', 'images/'.$fn);
+	} else {
+		my $fn = $_.'.png';
+		$fn =~ s/\//\./g;
+		$fn =~ s/www\d?\.//g;
+		system('curl', 'http://www.google.com/s2/favicons?domain='.$_, '-o', 'images/'.$fn);
+	}
+}
