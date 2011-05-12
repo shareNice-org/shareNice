14ffd6eacf980061e29b62eada64a5bb8a054e24
Monika <monika.stepinska@garlik.com>
2011-05-12 13:31:17 +0100

    very initial attempt to parse JSON configuration

--- shareNice.js
+++ shareNice.js
@@ -1,40 +1,31 @@
+var shareNiceConfig = '{ "twitter" : { "url" : "http://twitter.com/home?status=[SHARE_URL]" , "icon" : "twitter.com.png" }}';
+
+
 jQuery(document).ready(function() {
+    var config = jQuery.parseJSON(shareNiceConfig) ;
+//    alert(config.twitter.url);
+
+    var shareBoxHTML = '<ul id="socialLinks">' +
+                       '<li>' + 
+                       '<a href="' + config.twitter.url + '">' +
+                       '<img src="images/' + config.twitter.icon + '" style="border: 0" alt="email icon" />' +
+                       '</a>' +
+                       '</li>' +
+                       '<div style="clear:both;"></div>'+
+                       '</ul>';
+    $('div#share-box-body').html(shareBoxHTML);
+
     $('a#shareBoxLink, #share-box-wrapper').mouseenter(function() {
     $('#share-box-wrapper').show();
     $('#share-box-wrapper').css({'top':$(this).offset().top});
     $('#share-box-wrapper').css({'left':$(this).offset().left});
 
-    var value = window.location;
-    $('a#share-twitter').attr('href', 'http://twitter.com/home?status=' + value);
-    $('a#share-twitter').attr('target','_blank');
-    $('a#share-email').attr('href', 'mailto:?body=' + value);
-    $('a#share-fb').attr('href', 'http://www.facebook.com/sharer.php?u=' + value);
-    $('a#share-fb').attr('target','_blank');
     });
 
     $('#share-box-wrapper').mouseleave(function () {
     $(this).hide();
     });
+
 });
-
-var shareNiceConfig = '
-{ "twitter" : 
-    {
-        "url" : "http://twitter.com/home?status=[SHARE_URL]" ,
-        "icon" : "twitter.com.png" 
-    },
-  "email" : 
-    {
-        "url" : "mailto:?subject=Look&body=[SHARE_URL]",
-        "icon" : "email.png"
-    },
-  "facebook" : 
-    {
-        "url" : "http://www.facebook.com/sharer.php?u=[SHARE_URL]",
-        "icon" : "facebook.com.png"
-    }
-
-}';
-
 
 /* vi:set expandtab sts=4 sw=4: */
