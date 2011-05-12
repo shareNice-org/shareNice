65fa08237c93cef8c59f8b03af082f6dd013aa6a
Mischa Tuffield <mischa@mmt.me.uk>
2011-05-12 11:06:16 +0100

    Initial Commit of the shareNice sharing tool

---
+++ script.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function() {
+  $('a#shareBoxLink, #share-box-wrapper').mouseenter(function() {
+    $('#share-box-wrapper').show();
+    $('#share-box-wrapper').css({'top':$(this).offset().top});
+    $('#share-box-wrapper').css({'left':$(this).offset().left});
+
+    var value = window.location;
+    $('a#share-twitter').attr('href', 'http://twitter.com/home?status=' + value);
+    $('a#share-twitter').attr('target','_blank');
+    $('a#share-email').attr('href', 'mailto:?body=' + value);
+    $('a#share-fb').attr('href', 'http://www.facebook.com/sharer.php?u=' + value);
+    $('a#share-fb').attr('target','_blank');
+  });
+
+  $('#share-box-wrapper').mouseleave(function () {
+    $(this).hide();
+  });
+});
+
