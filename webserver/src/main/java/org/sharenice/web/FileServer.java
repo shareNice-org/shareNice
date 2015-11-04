4e7b99d796f90ba34384ecddef0be2a5efdca69b
Mischa Tuffield <mischa@mmt.me.uk>
2015-11-04 10:57:03 +0000

    First basic HTTP2 support

--- webserver/src/main/java/org/sharenice/web/FileServer.java
+++ webserver/src/main/java/org/sharenice/web/FileServer.java
@@ -1,28 +1,42 @@
 package org.sharenice.web;
 
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.http2.server.HTTP2CServerConnectionFactory;
+import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.server.handler.gzip.GzipHandler;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 public class FileServer {
 
     public static void main(String[] args) throws Exception {
-        // Create a basic Jetty server object that will listen on port 8080.  Note that if you set this to port 0
-        // then a randomly available port will be assigned that you can either look in the logs for the port,
-        // or programmatically obtain it for use in test cases.
-        Server server = new Server(8080);
 
-        // Create the ResourceHandler. It is the object that will actually handle the request for a given file. It is
-        // a Jetty Handler object so it is suitable for chaining with other handlers as you will see in other examples.
+        // The Jetty Server.
+        Server server = new Server();
+
+        // Common HTTP configuration.
+        HttpConfiguration config = new HttpConfiguration();
+
+        // HTTP/1.1 support.
+        HttpConnectionFactory http1 = new HttpConnectionFactory(config);
+
+        // HTTP/2 cleartext support.
+        HTTP2CServerConnectionFactory http2c = new HTTP2CServerConnectionFactory(config);
+
+        ServerConnector connector = new ServerConnector(server, http1, http2c);
+        connector.setPort(8080);
+        server.addConnector(connector);
+
+        // Here configure contexts / servlets / etc.
+
         ResourceHandler resource_handler = new ResourceHandler();
         // Configure the ResourceHandler. Setting the resource base indicates where the files should be served out of.
         // In this example it is the current directory but it can be configured to anything that the jvm has access to.
         resource_handler.setDirectoriesListed(true);
-        resource_handler.setWelcomeFiles(new String[]{"index.html"});
         resource_handler.setResourceBase("src/main/webapp");
+        resource_handler.setWelcomeFiles(new String[]{ "index.html" });
 
         // Add the ResourceHandler to the server.
         GzipHandler gzip = new GzipHandler();
@@ -31,10 +45,9 @@ public class FileServer {
         handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
         gzip.setHandler(handlers);
 
-        // Start things up! By using the server.join() the server thread will join with the current thread.
-        // See "http://docs.oracle.com/javase/1.5.0/docs/api/java/lang/Thread.html#join()" for more details.
         server.start();
         server.join();
+
     }
 
 
